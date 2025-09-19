a7a9edc2ec03106eb9cf99726e98fd82fd5873c2
Jose Miguel <jose@sulit.cl>
2025-09-19 16:41:27 -0500

    create and initial project configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs', 'dist', 'test/**', 'src/**/*.spec.ts'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      // '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+      // 'no-console': ['warn', { allow: ['error', 'warn', 'info'] }],
+    },
+  },
+);
